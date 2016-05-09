07b9ad40f1f57b20f69b616feb39a79771672c2f
Mikolaj Grzyb <mikolaj@codequest.com>
2016-05-09 22:21:13 +0200

    setup google plus, setup repo insert

--- web/controllers/auth_controller.ex
+++ web/controllers/auth_controller.ex
@@ -1,14 +1,16 @@
 require IEx
 defmodule Janitor.AuthController do
   use Janitor.Web, :controller
+  alias Janitor.User 
 
   def connect(conn, _params) do
     redirect conn, external: Google.authorize_url!(scope: "email profile")
   end
 
   def oauth(conn, %{"code" => code}) do 
-    user = token(code) |> get_user!
-    IEx.pry
+    params = token(code) |> get_user! |> map_params
+    changeset = User.registration_changeset(%User{}, params)
+    {:ok, user} = Repo.insert(changeset)
     redirect conn, external: "http://localhost:5080"
   end 
 
@@ -17,7 +19,13 @@ defmodule Janitor.AuthController do
   end
 
   defp get_user!(token) do
-    user_url = "https://www.googleapis.com/plus/v3/people/me/openIdConnect"
+    user_url = "https://www.googleapis.com/plus/v1/people/me"
     OAuth2.AccessToken.get!(token, user_url)
   end
+
+  defp map_params(data) do 
+    %{body: %{"emails" => [%{"value" => email}], "displayName" => name, "id" => id}} = data
+    [first_name, last_name] = String.split(name)
+    %{email: email, google_id: id, first_name: first_name, last_name: last_name}
+  end 
 end
---
+++ web/models/user.ex
@@ -0,0 +1,18 @@
+defmodule Janitor.User do
+  use Janitor.Web, :model
+
+  schema "users" do
+    field :first_name, :string
+    field :last_name, :string
+    field :email, :string
+    field :google_id, :string
+    timestamps
+  end
+
+  def registration_changeset(model, params \\ :empty) do 
+    model
+    |> cast(params, ~w(first_name email google_id), ~w(last_name))
+    |> validate_format(:email, ~r/^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/)
+  end 
+
+end
