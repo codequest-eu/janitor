72e7ab62c3f258201387fff8019ab9db97f64682
Przemek Bieliński <przemek@codequest.com>
2016-05-12 21:46:43 +0200

    Add AuthorizationPlug for JWT token verification

--- web/router.ex
+++ web/router.ex
@@ -7,13 +7,21 @@ defmodule Janitor.Router do
 
   pipeline :api do
     plug :accepts, ["json"]
+    plug Janitor.Plugs.Authorization
   end
 
   scope "/", Janitor do
-    pipe_through :browser # Use the default browser stack
+    pipe_through :browser
 
     get "/connect", AuthController, :connect
     get "/oauth", AuthController, :oauth
   end
 
+
+  scope "/api", Janitor do
+    pipe_through :api
+  end
+
+  defp authenticate_request do
+  end
 end
