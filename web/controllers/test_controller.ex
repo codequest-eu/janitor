6c9c3c3db112866042c4ac10c662b276dd13e363
Przemek Bieliński <przemek@codequest.com>
2016-05-12 22:27:06 +0200

    Authorisation plug refactor

---
+++ web/controllers/test_controller.ex
@@ -0,0 +1,7 @@
+defmodule Janitor.TestController do
+  use Janitor.Web, :controller
+
+  def index(conn, params) do
+    render "DUPA"
+  end
+end
--- web/router.ex
+++ web/router.ex
@@ -1,5 +1,8 @@
 defmodule Janitor.Router do
   use Janitor.Web, :router
+  use Timex
+  import Plug.Conn
+  import Joken
 
   pipeline :browser do
     plug :accepts, ["html"]
@@ -7,7 +10,7 @@ defmodule Janitor.Router do
 
   pipeline :api do
     plug :accepts, ["json"]
-    plug Janitor.Plugs.Authorization
+    plug :authenticate_request
   end
 
   scope "/", Janitor do
@@ -20,8 +23,30 @@ defmodule Janitor.Router do
 
   scope "/api", Janitor do
     pipe_through :api
+
+    get "/", TestController, :index
   end
 
-  defp authenticate_request do
+  def authenticate_request(conn, header_key) do
+    get_req_header(conn, %{header_key: "Authorization"}) |> String.split |> tl |> verify
+    |> check_expiration_time(conn)
+  end
+
+  defp check_expiration_time(conn, struct) do
+    if DateTime.today < struct[:claims][:exp] do
+      send_resp(conn, 403, struct)
+    else
+      assign_current_user(conn, struct)
+    end
+  end
+
+  defp assign_current_user(conn, struct) do
+    case Repo.get!(User, struct[:claims][:user_id]) do
+      {:ok, user} ->
+        user
+        #@current_user = user
+      :error ->
+        send_resp(conn, 403, struct)
+    end
   end
 end
