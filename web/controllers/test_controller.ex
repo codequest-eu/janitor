930f86d0b3137ce197788aa2b267e0171bf9f7c1
Przemek Bieliński <przemek@codequest.com>
2016-05-15 20:20:33 +0200

    Change JWT library, implement claims parsing

--- web/controllers/test_controller.ex
+++ web/controllers/test_controller.ex
@@ -1,7 +1,28 @@
 defmodule Janitor.TestController do
   use Janitor.Web, :controller
+  use Timex
 
-  def index(conn, params) do
-    render "DUPA"
+  def index(conn, _params) do
+    # check_expiration_time(conn[:claims])
+    assign_current_user(conn)
+  end
+
+  defp check_expiration_time(conn) do
+    claims = conn[:claims]
+    if DateTime.today < claims[:exp] do
+      send_resp(conn, 403, nil)
+    else
+      conn
+    end
+  end
+
+  defp assign_current_user(conn) do
+    claims = conn.assigns[:claims]
+    case Repo.get!(User, claims[:user_id]) do
+      {:ok, user} ->
+        IO.puts user
+      :error ->
+        send_resp(conn, 403, nil)
+    end
   end
 end
