2f2cce56d39ad00e75401d67e577cb83d9c2d32f
Przemek Bieliński <przemek@codequest.com>
2016-05-18 22:47:04 +0200

    Refactor :api pipeline, change token signature key to proper 32 bits.

--- web/controllers/auth_controller.ex
+++ web/controllers/auth_controller.ex
@@ -11,13 +11,15 @@ defmodule Janitor.AuthController do
   def oauth(conn, %{"code" => code}) do
     params = token(code) |> get_user! |> map_params
     changeset = User.registration_changeset(%User{}, params)
-    user = User.find_or_create(changeset)
-    token = JsonWebToken.sign(
-      %{user_id: user.id, exp: Timex.shift(Date.today, days: 7)},
-      %{key: System.get_env("JWT_SECRET")}
-    )
+    token = User.find_or_create(changeset) |> sign_jwt_token
     put_req_header(conn, "authorization", "bearer #{token}")
     redirect conn, to: "/api/test"
+  end
+
+  defp sign_jwt_token(user) do
+    JsonWebToken.sign(
+      %{user_id: user.id, exp: Timex.shift(Date.today, days: 7)},
+      %{key: System.get_env("JWT_SECRET")})
   end
 
   defp token(token_string) do
