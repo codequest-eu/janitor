4e6ad600b495e6652e6c9ae03e2bf67a156c1bed
Przemek Bieliński <przemek@codequest.com>
2016-05-26 19:30:54 +0200

    Refactor JWTVerifier

--- web/controllers/test_controller.ex
+++ web/controllers/test_controller.ex
@@ -1,6 +1,5 @@
 defmodule Janitor.TestController do
   use Janitor.Web, :controller
-  use Timex
 
   def index(conn, _params) do
     text conn, "Success!"
--- web/plugs/JWTverifier.ex
+++ web/plugs/JWTverifier.ex
@@ -7,18 +7,22 @@ defmodule Janitor.Plugs.JWTVerifier do
   end
 
   def call(conn, _) do
-    verified_token =
-      with {:ok, conn} <- verify_token(conn),
-        {:ok, conn} <- check_expiration_time(conn),
-        {:ok, conn} <- assign_current_user(conn),
-      do: {:ok, conn}
-    case verified_token do
+    case verification_chain(conn) do
       {:ok, conn} -> conn
       {:error, message} -> send_resp(conn, 403, message) |> Plug.Conn.halt
     end
   end
 
-  def verify_token(conn) do
+  #PRIVATE
+
+  defp verification_chain(conn) do
+    with {:ok, conn} <- verify_token(conn),
+      {:ok, conn} <- check_expiration_time(conn),
+      {:ok, conn} <- assign_current_user(conn),
+    do: {:ok, conn}
+  end
+
+  defp verify_token(conn) do
     token = parse_token(conn)
     case JsonWebToken.verify(token, %{key: System.get_env("JWT_SECRET")}) do
       {:ok, claims} ->
@@ -28,7 +32,7 @@ defmodule Janitor.Plugs.JWTVerifier do
     end
   end
 
-  def check_expiration_time(conn) do
+  defp check_expiration_time(conn) do
     claims = conn.assigns[:claims]
     if Date.today > claims[:exp] do
       {:error, "Unauthorized request"}
@@ -37,7 +41,7 @@ defmodule Janitor.Plugs.JWTVerifier do
     end
   end
 
-  def assign_current_user(conn) do
+  defp assign_current_user(conn) do
     claims = conn.assigns[:claims]
     case Repo.get!(User, claims[:user_id]) do
       {:ok, user} ->
