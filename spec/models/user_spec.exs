6a609f27cbf849863f942f8db2673de1d0931e92
Mikołaj Grzyb <mikolaj@codequest.com>
2016-06-11 12:21:35 +0200

    Create db helpers

--- spec/models/user_spec.exs
+++ spec/models/user_spec.exs
@@ -1,6 +1,6 @@
 defmodule Janitor.UserSpec do 
   import Ecto.Query
-  use ESpec.Phoenix, model: App.User
+  use ESpec.Phoenix, model: Janitor.User
   alias Janitor.User
 
   let :changeset, do: User.registration_changeset(%User{}, %{
@@ -32,34 +32,6 @@ defmodule Janitor.UserSpec do
       it "applies email error message" do 
         changeset.valid?
         expect(changeset.errors[:email]).to_not eq nil
-      end 
-    end 
-  end 
-
-  describe ".find_or_create" do 
-    context "when user is not in the database" do
-      let! :user, do: User.find_or_create(changeset)
-      
-      it "inserts user to DB" do 
-        query = from(p in User, select: count(p.id))
-        expect(Repo.one(query)).to eq 1
-      end 
-
-      it "returns new user" do
-        expect(user.id).to_not eq nil
-      end 
-    end 
-
-    context "when user is already in DB" do 
-      let! :user, do: User.find_or_create(changeset)
-
-      it "returns the user" do
-        expect(User.find_or_create(changeset).id).to eq user.id
-      end 
-
-      it "does not change count of objects" do 
-        query = from(p in User, select: count(p.id))
-        expect(Repo.one(query)).to eq 1
       end 
     end 
   end 
