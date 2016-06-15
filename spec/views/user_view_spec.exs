3d35f3980bdaee0b55840f75226ef6ef5d162605
Przemek Bieliński <przemek@codequest.com>
2016-06-15 10:09:12 +0200

    Refactor UserView spec

--- spec/views/user_view_spec.exs
+++ spec/views/user_view_spec.exs
@@ -5,9 +5,13 @@ defmodule Janitor.UserViewsSpec do
 
   describe "me" do
     let :user, do: create(:user)
-    let :user_to_json, do: Map.take(user, [:first_name, :last_name, :email])
+    let :user_to_json do
+      user
+      |> Map.take([:first_name, :last_name, :email])
+      |> Poison.encode!
+    end
     subject do: render("me.json", user: user)
 
-    it do: should eq(Poison.encode!(user_to_json))
+    it do: should eq(user_to_json)
   end
 end
