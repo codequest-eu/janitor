ce52577312dca97fea6ddf9b869541ba258a61a1
Mikołaj Grzyb <mikolaj@codequest.com>
2016-06-19 13:18:41 +0200

    Use factories in user_spec

--- spec/models/user_spec.exs
+++ spec/models/user_spec.exs
@@ -2,13 +2,10 @@ defmodule Janitor.UserSpec do
   import Ecto.Query
   use ESpec.Phoenix, model: Janitor.User
   alias Janitor.User
+  alias Janitor.UserFactory
 
-  let :changeset, do: User.registration_changeset(%User{}, %{
-    first_name: "Mark",
-    last_name: "test", 
-    google_id: "test1234", 
-    email: "test@test.com"
-  })
+  let :changeset, do: User.registration_changeset(%User{}, 
+    UserFactory.fields_for(:user))
 
   describe "validation" do 
     context "valid changeset" do 
@@ -18,12 +15,8 @@ defmodule Janitor.UserSpec do
     end 
 
     context "invalid changeset" do 
-      let :changeset, do: User.registration_changeset(%User{}, %{
-        first_name: "Mark",
-        last_name: "test", 
-        google_id: "test1234", 
-        email: "wrong_email"
-      })
+      let :changeset, do: User.registration_changeset(%User{}, 
+        UserFactory.fields_for(:user, %{email: 'wrong_email'}))
 
       it "sets valid? to false" do 
         expect(changeset.valid?).to eq false
