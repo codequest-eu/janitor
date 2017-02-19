b3d401113e6f95a74d7d7c03fe61ca7aa480c155
Mikołaj Grzyb <mikolaj@codequest.com>
2017-02-19 13:58:39 +0100

    Fix elixir warnings

--- lib/tasks/mix_days_creator.ex
+++ lib/tasks/mix_days_creator.ex
@@ -3,15 +3,12 @@ defmodule Mix.Tasks.MixDaysCreator do
 
   @shortdoc 'Create days for month - [date]'
   def run(_) do
+    IO.puts "START"
     Janitor.Repo.start_link()
     today = Ecto.Date.utc
-    case (today.month + 1) == 13 do
-      true ->
-        year = (today.year + 1)
-        month = 1
-      false ->
-        year = today.year
-        month = (today.month + 1)
+    {month, year} = case (today.month + 1) == 13 do
+      true -> {1, (today.year + 1)}
+      false -> {(today.month + 1), today.year}
     end
     date = Ecto.Date.cast!(%{year: year, month: month, day: 1})
     Janitor.CreateDaysForMonth.call(date)
